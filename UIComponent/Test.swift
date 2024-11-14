e03c7a0e76fe141fe24e2912b2050ae3d47494cb
Lam4w <nguyentunglam4w@gmail.com>
2024-11-14 10:16:35 +0700

    update: currency format test ui

--- UIComponent/Test.swift
+++ UIComponent/Test.swift
@@ -8,11 +8,40 @@
 import SwiftUI
 
 struct Test: View {
+    @State var amount: Decimal = 0.0
+    
     var body: some View {
-        Text("hello world")
+        VStack {
+            // Default the currency to match the users locale,
+            // or fall back to a certain if we can't figure out the locale
+            Text(amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+            // Displays "$0.00"
+                
+            // We can ensure our text to only ever display a certain type
+            // of currency by using the following line (instead of the one above)
+            Text(amount, format: .currency(code: "GBP"))
+            // Displays ￡0.00 (no matter what locale you have choosen)
+                
+            // Accepting a number as input to a text field
+            TextField("Amount", value: $amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+        }
+        .padding()
     }
 }
 
 #Preview {
     Test()
 }
+
+
+// import SwiftUI
+
+// struct ContentView: View {
+//     @State var amount: Decimal = 0.0
+    
+//     var body: some View {
+//         Text(amount, format: .currency(code: "GBP"))
+//         TextField("Amount", value: $amount, format: .number.precision(.fractionLength(2)))
+//             .keyboardType(.decimalPad)
+//     }
+// }
