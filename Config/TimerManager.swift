f2bfac1d202b6995453578e258c75e99167d21b8
Lam4w <nguyentunglam4w@gmail.com>
2024-12-19 14:04:33 +0700

    update: add timer manager

--- Config/TimerManager.swift
+++ Config/TimerManager.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+class TimerManager: ObservableObject {
+    @Published var timerAction: String = "Waiting..."
+    private var timer: DispatchSourceTimer?
+
+    func startTimer() {
+        let queue = DispatchQueue.global(qos: .userInitiated)
+        timer = DispatchSource.makeTimerSource(queue: queue)
+        timer?.schedule(deadline: .now(), repeating: 10.0)
+
+        timer?.setEventHandler { [weak self] in
+            DispatchQueue.main.async {
+                self?.timerAction = "Timer fired at \(Date())"
+            }
+        }
+
+        timer?.resume()
+    }
+
+    func stopTimer() {
+        timer?.cancel()
+        timer = nil
+    }
+}
