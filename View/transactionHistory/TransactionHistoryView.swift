a940a4049f70b952ad43862b93cb032ea026c570
Lam4w <cuongnh>
2024-12-25 15:57:19 +0700

    update

---
+++ View/transactionHistory/TransactionHistoryView.swift
@@ -0,0 +1,18 @@
+//
+//  TransactionHistoryView.swift
+//  cpqr_merchant_demo
+//
+//  Created by Macbook on 25/12/24.
+//
+
+import SwiftUI
+
+struct TransactionHistoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TransactionHistoryView()
+}
